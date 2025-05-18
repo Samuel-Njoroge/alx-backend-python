4f2e435e55d580d3fd2469b3250f87311f56b5d3
NJ <njorogekiragu8@gmail.com>
2025-05-18 16:09:50 +0300

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -4,8 +4,8 @@ def stream_users_in_batches(batch_size):
     """Yields batches of users from the user_data table."""
     connection = mysql.connector.connect(
         host='localhost',
-        user='root',        # Change if needed
-        password='',        # Change if needed
+        user='root',
+        password='mypassword',
         database='ALX_prodev'
     )
     cursor = connection.cursor(dictionary=True)
@@ -29,7 +29,6 @@ def batch_processing(batch_size):
     """Yields filtered users over age 25 from each batch."""
     for batch in stream_users_in_batches(batch_size):
         yield [user for user in batch if float(user['age']) > 25]
-
 
 
 if __name__ == "__main__":
